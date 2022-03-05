645946d1ce04ed0875ec9cf1a388566102b99dfa
Tiago <tiagoperroni86@gmail.com>
2022-03-05 19:03:17 -0300

    alterado a porta do server

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server on port 3000!");
+app.listen(3001, () => {
+  console.log("Server on port 3001!");
 });
 
 module.exports = app;
